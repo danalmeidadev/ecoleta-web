b229eba498e6b163e33077dcf555d9d162369f80
danalmeidadev <devdanalmeida@gmail.com>
2020-07-14 13:52:00 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Global/App.css';
+import Routes from './routes/routes';
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Routes />
+    </>
+  );
+};
+
+export default App;
